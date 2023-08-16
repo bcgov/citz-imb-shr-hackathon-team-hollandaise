e5223f49f2dee24d16001ed09042921be38095fd
Noor Chasib <noor.chasib@gov.bc.ca>
2023-08-16 16:07:53 -0700

    implement update navbar with login and logout functionality

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -6,13 +6,23 @@ const Navbar = () => {
   const user = authState.userInfo;
 
   return (
-    <nav className="flex h-20 w-full items-center border-b-2 border-yellow-500 bg-blue-500">
-      <img className="ml-8 h-full" alt="logo" src={Logo} />
-      <p className="ml-5 text-xl text-white">Team Hollandaise</p>
+    <nav className="flex h-20 w-full items-center border-b-2 border-yellow-500 bg-blue-500 justify-between">
+      <img className="h-full ml-8" alt="logo" src={Logo} />
+      <p className="text-xl text-white mr-20">Team Hollandaise</p>
       {user ? (
-        <button onClick={() => (window.location.href = getLogoutURL())}>Logout</button>
+        <button
+          onClick={() => (window.location.href = getLogoutURL())}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 mr-8"
+        >
+          Logout
+        </button>
       ) : (
-        <button onClick={() => (window.location.href = getLoginURL())}>Login</button>
+        <button
+          onClick={() => (window.location.href = getLoginURL())}
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-8"
+        >
+          Login
+        </button>
       )}
     </nav>
   );
