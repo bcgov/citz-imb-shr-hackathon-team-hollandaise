2005a0a8630439f2e37d92d7ccc926cae3513d81
Noor Chasib <noor.chasib@gov.bc.ca>
2023-08-17 11:57:36 -0700

    move provider to main tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,23 +3,18 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Authenticated from '@/components/Authenticated';
 import '@/index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <Router>
       <div className="app flex min-h-screen flex-col">
-        <QueryClientProvider client={queryClient}>
-          <Navbar />
-          <div className="content m-8 mb-16 flex flex-grow items-center justify-center">
-            <Routes>
-              <Route path="/" element={<Authenticated />} />
-            </Routes>
-          </div>
-          <Footer />
-        </QueryClientProvider>
+        <Navbar />
+        <div className="content m-8 mb-16 flex flex-grow items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Authenticated />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { KeycloakProvider } from '@bcgov/keycloak-react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <KeycloakProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </KeycloakProvider>
   </React.StrictMode>,
 );
