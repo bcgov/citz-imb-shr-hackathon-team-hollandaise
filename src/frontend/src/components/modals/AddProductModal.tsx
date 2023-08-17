3459f58372f628206faee17cc35f32eb074fdba6
Noor Chasib <noor.chasib@gov.bc.ca>
2023-08-17 15:15:10 -0700

    fix modal

--- src/frontend/src/components/modals/AddProductModal.tsx
+++ src/frontend/src/components/modals/AddProductModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, TextInput } from '@mantine/core';
+import { Button, Modal, TextInput, Textarea, ScrollArea } from '@mantine/core';
 import { DatePickerInput } from '@mantine/dates';
 import { useState } from 'react';
 
@@ -11,16 +11,24 @@ const AddProductModal = ({ opened, onClose }) => {
       opened={opened}
       onClose={onClose}
       withCloseButton={true}
-      title="Create New Product"
+      title="Create New"
       size="md"
       padding="md"
+      scrollAreaComponent={ScrollArea.Autosize}
     >
-      <TextInput placeholder="Position Number" label="Position Number" />
+      <TextInput placeholder="Name" label="Name" />
+      <TextInput placeholder="Current Classification" label="Current Classification" />
+      <TextInput placeholder="Current Job Title" label="Current Job Title" />
       <TextInput placeholder="Hiring Manager" label="Hiring Manager" />
       <TextInput placeholder="Position Number" label="Position Number" />
-      <TextInput placeholder="Hiring Manager" label="Hiring Manager" />
-      <TextInput placeholder="Position Number" label="Position Number" />
-      <TextInput placeholder="Hiring Manager" label="Hiring Manager" />
+      <TextInput placeholder="Current Salary" label="Current Salary" />
+      <TextInput placeholder="Current Organization" label="Current Organization" />
+      <TextInput placeholder="Education Experience" label="Education Experience" />
+      <TextInput placeholder="Proposed Job Title" label="Proposed Job Title" />
+      <TextInput placeholder="Proposed Classification" label="Proposed Classification" />
+      <TextInput placeholder="Proposed Appointment" label="Proposed Appointment" />
+      <TextInput placeholder="Proposed Salary" label="Proposed Salary" />
+      <Textarea placeholder="Comments" label="Comments" />
       <DatePickerInput
         valueFormat="YYYY-MM-DD"
         label="Date Submitted"
