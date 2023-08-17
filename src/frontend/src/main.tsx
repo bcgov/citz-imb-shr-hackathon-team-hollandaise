35abda20c2c186063e96bc20231f7b48d68da23b
Noor Chasib <noor.chasib@gov.bc.ca>
2023-08-17 11:00:27 -0700

    fix improper import statement

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { KeycloakProvider } from '@bcgov/keycloak-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
-import '@index.css';
+import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
